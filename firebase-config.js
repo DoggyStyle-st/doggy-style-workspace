5fa488237ee63842fd162a70b4021d8b956a83c4
Doggy Style <h5v6j78dsd@privaterelay.appleid.com>
2026-01-10 22:16:28 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,15 @@
 // firebase-config.js
-// Wird von app.js / ANA035_MINLOGIN genutzt.
-// Enthält die Firebase Web-App Konfiguration (aus Firebase Console -> Projekteinstellungen -> SDK-Konfiguration).
+// WICHTIG: Diese Datei enthält die Firebase Web-App Konfiguration.
+// Sie ist NICHT geheim, aber muss korrekt sein.
 
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD70s8yl8EEFguvy5nEi27Q-NaF1BA8IJ8",
+  apiKey: "AIzaSyD7Os8yl8FEFquvv5nEj270-NaF1BA8IJ8",
   authDomain: "doggy-style-hundepension.firebaseapp.com",
   projectId: "doggy-style-hundepension",
   storageBucket: "doggy-style-hundepension.firebasestorage.app",
   messagingSenderId: "407371827200",
   appId: "1:407371827200:web:b51a856d20617dd9f070e5"
 };
+
+// (Optional) Kompatibilität: falls irgendwas noch window.firebaseConfig erwartet
+window.firebaseConfig = window.FIREBASE_CONFIG;
