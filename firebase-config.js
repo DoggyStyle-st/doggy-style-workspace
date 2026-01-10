1e44912c66bf686a42975b7a0207b0a0a60d9f3a
Doggy Style <h5v6j78dsd@privaterelay.appleid.com>
2026-01-10 14:50:17 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,20 @@
-// firebase-config.js
-// WICHTIG: Hier gehören DEINE Firebase Keys rein.
-// Wenn du diese Datei schon im Repo hast, bitte NICHT überschreiben.
-window.FIREBASE_CONFIG = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+// Firebase Konfiguration (Doggy Style Workspace)
+// Hinweis: Diese Datei wird vor auth.js/app.js geladen.
+// Du kannst hier später weitere Admin-Emails ergänzen.
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyD7Os8yl8FEFquvv5nEj270-NaF1BA8IJ8",
+  authDomain: "doggy-style-hundepension.firebaseapp.com",
+  projectId: "doggy-style-hundepension",
+  storageBucket: "doggy-style-hundepension.firebasestorage.app",
+  messagingSenderId: "407371827200",
+  appId: "1:407371827200:web:b51a856d20617dd9f070e5"
 };
+
+// frei wählbar (wird als "Mandant" / Hof-Ordner genutzt)
+window.firebaseOrgId = "doggystyle";
+
+// Admin-Whitelist (du kannst Anschi hier später ergänzen)
+window.firebaseAdminEmails = [
+  "raphael@boch-plan.de"
+];
