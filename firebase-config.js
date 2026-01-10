069f92842b8979a854fb846f2d782b7301036a20
Doggy Style <h5v6j78dsd@privaterelay.appleid.com>
2026-01-10 17:13:50 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,20 @@
-// Firebase Konfiguration (Doggy Style Workspace)
-// Hinweis: Diese Datei wird vor auth.js/app.js geladen.
-// Du kannst hier später weitere Admin-Emails ergänzen.
+// firebase-config.js
+// Diese Datei wird von app.js als window.firebaseConfig / window.firebaseAdminEmails erwartet.
+// Sie darf PUBLIC sein (Firebase Web Config), aber bitte KEINE privaten Schlüssel hier ablegen.
 
-window.firebaseConfig = {
-  apiKey: "AIzaSyD7Os8yl8FEFquvv5nEj270-NaF1BA8IJ8",
-  authDomain: "doggy-style-hundepension.firebaseapp.com",
-  projectId: "doggy-style-hundepension",
-  storageBucket: "doggy-style-hundepension.firebasestorage.app",
-  messagingSenderId: "407371827200",
-  appId: "1:407371827200:web:b51a856d20617dd9f070e5"
-};
+(function () {
+  window.firebaseConfig = {
+    apiKey: "AIzaSyA-HTcgwj8_8WKyUuCBp2TFkCWv_XkHE_M",
+    authDomain: "doggy-style-hundepension.firebaseapp.com",
+    projectId: "doggy-style-hundepension",
+    storageBucket: "doggy-style-hundepension.firebasestorage.app",
+    messagingSenderId: "980060343319",
+    appId: "1:980060343319:web:2aa36499b357c93207017b"
+  };
 
-// frei wählbar (wird als "Mandant" / Hof-Ordner genutzt)
-window.firebaseOrgId = "doggystyle";
-
-// Admin-Whitelist (du kannst Anschi hier später ergänzen)
-window.firebaseAdminEmails = [
-  "raphael@boch-plan.de"
-];
+  // Admin‑Whitelist (für Rollen/Features). In app.js wird diese Liste verwendet.
+  window.firebaseAdminEmails = [
+    "info@wildwestallgaeu-alpaka.de",
+    "r.boch@boch-plan.de"
+  ];
+})();
