7223f58749321596e34ed9e103e24dac8a115547
Doggy Style <h5v6j78dsd@privaterelay.appleid.com>
2026-01-11 20:12:01 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// ANA034_CLEAN minimal Service Worker (no aggressive caching)
-self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (e) => { /* passthrough */ });
