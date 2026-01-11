7b0eda32193d23ff39aff3ee836e605a65f2f4f0
Doggy Style <h5v6j78dsd@privaterelay.appleid.com>
2026-01-11 13:03:35 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -18,12 +18,3 @@ window.firebaseOrgId = "doggystyle";
 window.firebaseAdminEmails = [
   "raphael@boch-plan.de"
 ];
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD7Os8yl8FEFquvv5nEj270-NaF1BA8IJ8",
-  authDomain: "doggy-style-hundepension.firebaseapp.com",
-  projectId: "doggy-style-hundepension",
-  storageBucket: "doggy-style-hundepension.firebasestorage.app",
-  messagingSenderId: "407371827200",
-  appId: "1:407371827200:web:b51a856d20617dd9f070e5"
-};
