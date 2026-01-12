1efa78217cdfd83e6ac21ffd20b95640c18b5663
Doggy Style <h5v6j78dsd@privaterelay.appleid.com>
2026-01-12 12:20:31 +0100

    Delete auswertungen_ANA012.js

--- auswertungen_ANA012.js
+++
@@ -1,4 +0,0 @@
-// KPI-Berechnung & Filterlogik (E1 – Rechnungsdatum)
-document.getElementById('btn-pdf-report')?.addEventListener('click', () => {
-  if (window.generatePdfReport) window.generatePdfReport();
-});
